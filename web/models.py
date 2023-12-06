2e53de6016071a78f08020768d1147ac08b03de0
Koryackovae <koryackovae@yandex.ru>
2023-12-06 13:40:02 +0300

    + models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class EventSlotTag(models.Model):
+    title = models.CharField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class EventSlot(models.Model):
+    title = models.CharField(max_length=256)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    is_realtime = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(EventSlotTag)
