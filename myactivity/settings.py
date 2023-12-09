e760b4d0707cfb206cc4098a95688494d340d083
Koryackovae <koryackovae@yandex.ru>
2023-12-09 12:36:14 +0300

    + postgres connection

--- myactivity/settings.py
+++ myactivity/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
          'NAME': 'myactivity',
          'USER': 'myactivity',
          'PASSWORD': 'myactivity',
-         'HOST': '127.0.0.1',
+         'HOST': 'localhost',
          'PORT': '5432'
      }
 }
