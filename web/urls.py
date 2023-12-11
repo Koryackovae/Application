6a4420e0f82d3d33ba1c9b14dd6dd97cb6ac9b9b
Koryackovae <koryackovae@yandex.ru>
2023-12-11 16:05:51 +0300

    + http

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from web.views import main_veiw
+from web.views import main_view, registration_view
 
 urlpatterns = [
-    path('', main_veiw),
+    path('', main_view),
+    path("registration/", registration_view)
 ]
--- web/views.py
+++ web/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
-def main_veiw(request):
+from web.forms import RegistrationForm
+
+
+def main_view(request):
     return render(request, "web/main.html")
+
+
+def registration_view(request):
+    form = RegistrationForm()
+    return render(request, "web/registration.html", {"form": form})
