be37678c489582401cd8161904c29aa40d95558d
Koryackovae <koryackovae@yandex.ru>
2023-12-12 12:26:39 +0300

    +base templlate

--- web/views.py
+++ web/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-from django.contrib.auth import get_user_model
-from web.forms import RegistrationForm
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model, authenticate, login
+from web.forms import RegistrationForm, AuthForm
 
 User = get_user_model()
 
@@ -24,3 +23,16 @@ def registration_view(request):
             is_success = True
             print(form.cleaned_data)
     return render(request, "web/registration.html", {"form": form, "is_success": is_success})
+
+def auth_view(request):
+    form = AuthForm()
+    if request.method == 'POST':
+        form = AuthForm(data=request.POST)
+        if form.is_valid():
+            user = authenticate(**form.cleaned_data)
+            if user is None:
+                form.add_error(None, "Введены неверные данные")
+            else:
+                login(request, user)
+                return redirect("main")
+    return render(request, "web/auth.html", {'form': form})
